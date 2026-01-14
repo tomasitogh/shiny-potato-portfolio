4b94c2f9f413a0c14878c7f0a6bacb2b6f2e0e9e
Tomas Gonzalez Humphreys <tomasgonzalezhu.phreys12@gmail.com>
2026-01-13 23:59:15 -0300

    Se ajustaron colores para los themes y se agregaron botones en Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import { useGlobal } from "../context/GlobalContext"
+import "./Home.css"
 
 export default function Home() {
   const { t } = useGlobal()
 
   return (
-    <main className="page">
-      <h1>{t("homePage.title")}</h1>
-      <p>
+    <main className="page home-container">
+      <h1 className="home-title">{t("homePage.title")}</h1>
+      <p className="home-description">
         {t("homePage.description")}
       </p>
+      <div className="home-buttons">
+        <button className="btn-primary">Button 1</button>
+        <button className="btn-secondary">Button 2</button>
+      </div>
     </main>
   )
 }
